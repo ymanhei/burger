fff19409fc33c008db20cbb2f2adc1572d91a325
Shing Chan <ymanhei@gmail.com>
2020-01-10 15:14:29 +0800

    fisrt draft3

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWDB_URL);
+}
+else {
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
   });
+}
+
   
   connection.connect(function(err) {
     if (err) {
