402bf896cf055ff830cfbb89d339762550af91a4
Shing Chan <ymanhei@gmail.com>
2020-01-10 15:21:22 +0800

    fisrt draft3

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@
 
 var mysql = require("mysql");
 
-if (process.env.JAWDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWDB_URL);
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
   var connection = mysql.createConnection({
